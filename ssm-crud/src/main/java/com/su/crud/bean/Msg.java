b4556ce8d1fa5172cb541bc813f061082a81f3bc
suyinli <573055373@qq.com>
2019-08-15 10:42:17 +0800

    添加公共返回类，包含状态信息

---
+++ ssm-crud/src/main/java/com/su/crud/bean/Msg.java
@@ -0,0 +1,68 @@
+package com.su.crud.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.pagehelper.PageInfo;
+
+/**
+ * 通用的返回类
+ * @author ER
+ *
+ */
+public class Msg {
+	//状态码，自定义，200代表服务器处理成功
+	// 500代表服务器处理失败
+	private  int code ;
+	
+	//提示信息
+	private	 String msg;
+	
+	//用户要返回给浏览器的数据
+	private Map<String , Object> extend = new HashMap<String ,Object>();
+
+	public static Msg success() {
+		Msg result = new Msg();
+		result.setCode(200);
+		result.setMsg("处理成功！");
+		return result ;
+	}
+	
+	public static Msg fail() {
+		Msg result = new Msg();
+		result.setCode(400);
+		result.setMsg("处理失败！");
+		return result ;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Map<String, Object> getExtend() {
+		return extend;
+	}
+
+	public void setExtend(Map<String, Object> extend) {
+		this.extend = extend;
+	}
+
+	public Msg add(String key, Object value) {
+		// TODO Auto-generated method stub
+		this.getExtend().put(key,value);
+		return this ;
+	}
+	
+}
