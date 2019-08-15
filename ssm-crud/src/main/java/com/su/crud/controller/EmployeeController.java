fa2dd36e03e56bf31c6eba7ebd807e5858e32263
suyinli <573055373@qq.com>
2019-08-15 10:38:10 +0800

    将发送pageInfo修改为发送json请求

--- ssm-crud/src/main/java/com/su/crud/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/su/crud/controller/EmployeeController.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.su.crud.bean.Employee;
+import com.su.crud.bean.Msg;
 import com.su.crud.service.EmployeeService;
 
 /**
@@ -29,22 +30,35 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
-	@RequestMapping("/emps")
-	public String getEmps(@RequestParam(value="pn",defaultValue = "1")Integer pn ,Model model) {
-		//从外面传入一个pn查询的第几页
-		
-		
-		PageHelper.startPage(pn,5);//查询的页码以及每页的大小
+	//使responsebody工作必须导入jackson包
+	@RequestMapping("emps")
+	@ResponseBody
+	public Msg getEmpsWithJson(@RequestParam(value="pn",defaultValue = "1")Integer pn ) {
+PageHelper.startPage(pn,5);//查询的页码以及每页的大小
 		
 		List<Employee> emps=employeeService.getAll();
 		//分页,将数据分页之后存储在pageinfo中交给页面
 		//封装了详细的页面信息和数据
 		PageInfo page =new PageInfo(emps,5);//5为连续显示的页数右下角
-		model.addAttribute("pageInfo",page);//将查到的数据和分页信息发送给页面
-		
-		
-		return "list";
+		return Msg.success().add("pageInfo", page) ;
 	}
+	
+	
+	/*
+	 * @RequestMapping("/emps") public String
+	 * getEmps(@RequestParam(value="pn",defaultValue = "1")Integer pn ,Model model)
+	 * { //从外面传入一个pn查询的第几页
+	 * 
+	 * 
+	 * PageHelper.startPage(pn,5);//查询的页码以及每页的大小
+	 * 
+	 * List<Employee> emps=employeeService.getAll(); //分页,将数据分页之后存储在pageinfo中交给页面
+	 * //封装了详细的页面信息和数据 PageInfo page =new PageInfo(emps,5);//5为连续显示的页数右下角
+	 * model.addAttribute("pageInfo",page);//将查到的数据和分页信息发送给页面
+	 * 
+	 * 
+	 * return "list"; }
+	 */
 
 
 	
